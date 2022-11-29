cb5c5953413f22c903b3af42c5378b9ce1a62a62
BuildTools <unconfigured@null.spigotmc.org>
2022-11-29 12:44:36 +0100

    repetition

---
+++ first_num.py
@@ -0,0 +1,15 @@
+list=[1, 2, 3, 5, 6, 11, 12, 18, 19, 21]
+
+first_num=[]
+
+for i in list:
+    x=0
+    for a in range(2,i):
+        if i%a==0:
+            x=1
+    if x!=1:
+        first_num.append(i)
+    if i==1:
+        first_num.remove(i)
+        
+print(first_num)
